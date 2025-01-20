6278a61952842674db826bf7eb7053cd36b48762
Aarav Shukla <aarav.shukla105@gmail.com>
2025-01-20 23:59:29 +0530

    Update the remaining files

--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,44 @@
 const { Router } = require("express");
+const { userModel } = require("../db");
+const jwt = require("jsonwebtoken");
+const JWT_USER_PASSWORD = "hadijdfug"; 
+
 
 const userRouter = Router();
 
-userRouter.post("/signup", function (req, res) {
+userRouter.post("/signup", async function (req, res) {
+    const { email, password, firstName, lastName } = req.body; //TODO: adding zod validation
+    //TODO: hash the password in the plaintext pw is not stored in the DB
+
+
+    try {
+        await userModel.create({
+            email: email,
+            password: password,
+            firstName: firstName,
+            lastName: lastName
+        })
+    } catch (e) {
+        console.error(e);
+        res.status(500).json({
+            message: "signup failed"
+        })
+        return;
+    }
+
     res.json({
-        message: "sigup endpoint"
+        message: "sigup Succeeded"
     })
 })
 
-userRouter.post("/signin", function (req, res) {
+userRouter.post("/signin", async function (req, res) {
+    const { email, password } = req.body;
+
+    const user = await userModel.find({
+        email: email,
+        password: password
+    });
+
     res.json({
         message: "signin endpoint"
     })
